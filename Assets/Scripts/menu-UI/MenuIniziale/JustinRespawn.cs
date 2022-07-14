cf5918da915be51c7dd85a35565358d5f7903464
AndreaGarini <93157298+AndreaGarini@users.noreply.github.com>
2022-07-14 04:50:09 +0200

    città finita, restano solo asset da aggiungere

--- Assets/Scripts/menu-UI/MenuIniziale/JustinRespawn.cs
+++ Assets/Scripts/menu-UI/MenuIniziale/JustinRespawn.cs
@@ -2,25 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JustinRespawn : MonoBehaviour
+namespace SlimUI.ModernMenu
 {
-    public GameObject justin;
-    // Start is called before the first frame update
-    void Start()
+    public class JustinRespawn : MonoBehaviour
     {
+        public GameObject justin;
+        public GameObject menu;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    public void respawn()
-    {
-        StartCoroutine(respawnCoroutine());
-    }
+        public void respawn()
+        {
+            StartCoroutine(respawnCoroutine());
+        }
 
-    public IEnumerator respawnCoroutine()
-    {
-        yield return new WaitForSeconds(1f);
-        GameObject go = Instantiate(justin, transform.position, transform.rotation);
-        go.GetComponent<JustinManagerMenu>().enabled = true;
+        public IEnumerator respawnCoroutine()
+        {
+            yield return new WaitForSeconds(1f);
+            GameObject go = Instantiate(justin, transform.position, transform.rotation);
+            go.GetComponent<JustinManagerMenu>().enabled = true;
 
+        }
     }
 }
