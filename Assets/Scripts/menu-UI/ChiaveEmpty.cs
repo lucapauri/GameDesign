ea97e60eb7fd905863d52be9b563192507bb9583
lucapauri <luca.pauri9@gmail.com>
2022-07-15 23:58:45 +0200

    Tre dialoghi situazione stabile per eventuale ripristino

--- Assets/Scripts/menu-UI/ChiaveEmpty.cs
+++ Assets/Scripts/menu-UI/ChiaveEmpty.cs
@@ -9,8 +9,8 @@ public class ChiaveEmpty : MonoBehaviour
     private GameObject justin;
     private Scritte scritte;
     private bool isActive;
+    private bool isTaken;
     //per dialogo
-    public Animator justinAnim;
     public NPCConversation conversation;
     public DialogCamera dialogCamera;
     private GlobalVariables globalVariables;
@@ -29,6 +29,7 @@ public class ChiaveEmpty : MonoBehaviour
         scritte = FindObjectOfType<Scritte>();
         justin = GameObject.FindGameObjectWithTag("Player");
         isActive = false;
+        isTaken = false;
     }
 
     // Update is called once per frame
@@ -38,7 +39,7 @@ public class ChiaveEmpty : MonoBehaviour
         {
             justin = GameObject.FindGameObjectWithTag("Player");
         }
-        if (justin != null && Vector3.Distance(gameObject.transform.position, justin.transform.position) < 10)
+        if (justin != null && Vector3.Distance(gameObject.transform.position, justin.transform.position) < 10 && !isTaken)
         {
             scritte.setActive("Premi X per raccogliere la chiave", null);
             isActive = true;
@@ -51,13 +52,68 @@ public class ChiaveEmpty : MonoBehaviour
         if (isActive && Input.GetKeyDown(KeyCode.X))
         {
             scritte.setNotActive();
-            cameraOn = true;
+            isTaken = true;
+            StartCoroutine(conversationWaitCoroutine(2f));
         }
         if (cameraOn && Input.GetKeyDown(KeyCode.Return))
         {
             ConversationManager.Instance.PressSelectedOption();
             cameraOn = false;
+            endDialog();
         }
     }
 
+    private void endDialog()
+    {
+        globalVariables.justin.GetComponent<Justin>().enabled = true;
+        foreach (simpleEnemy enemy in globalVariables.enemies)
+        {
+            enemy.GetComponent<simpleEnemy>().enabled = true;
+        }
+
+        FindObjectOfType<UpCamera>().gameObject.GetComponent<Camera>().enabled = true;
+        FindObjectOfType<DownCamera>().gameObject.GetComponent<Camera>().enabled = true;
+        dialogCamera.GetComponent<Camera>().enabled = false;
+
+        dialogCamera.transform.position = startPos;
+
+        ConversationManager.Instance.EndConversation();
+        GameObject canvas = GameObject.FindGameObjectWithTag("Dialog");
+        Canvas can = canvas.GetComponent<Canvas>();
+    }
+
+    private void dialogSequence()
+    {
+        globalVariables.justin.GetComponent<Justin>().enabled = false;
+        globalVariables.justin.GetComponent<Animator>().SetBool("Walk", false);
+        foreach (simpleEnemy enemy in globalVariables.enemies)
+        {
+            enemy.GetComponent<simpleEnemy>().enabled = false;
+        }
+
+        dialogCamera.GetComponent<Camera>().enabled = true;
+        FindObjectOfType<UpCamera>().gameObject.GetComponent<Camera>().enabled = false;
+        FindObjectOfType<DownCamera>().gameObject.GetComponent<Camera>().enabled = false;
+
+        dialogCamera.moveCamera();
+        
+        StartCoroutine(conversationCoroutine(dialogCamera.moveTime));
+    }
+
+    private IEnumerator conversationWaitCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        dialogSequence();
+
+
+    }
+
+    private IEnumerator conversationCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        cameraOn = true;
+        ConversationManager.Instance.StartConversation(conversation);
+
+
+    }
 }
